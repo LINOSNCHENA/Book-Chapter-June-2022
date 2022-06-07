c5aab51c05a6bdc9faffba307198fe95648f3fce
Linos Nchena <linosnchena@gmail.om>
2022-06-07 19:16:12 +0200

    graphs

--- 3Ucihar/datatable.py
+++ 3Ucihar/datatable.py
@@ -6,15 +6,11 @@ import pandas as pd
 pd.set_option('max_colwidth', 10)
 pd.options.display.precision = 3
 plt.rcParams['figure.figsize'] = [15, 8]  # Plot-frame
-# plt.rcParams["figure.autolayout"] = True
+plt.rcParams["figure.autolayout"] = True
 plt.rcParams.update({'font.size': 29})  # Inside
-fondSizing = 15  # SupTitle & Xlabel
-recordsHieght = 14
+fontSizing = 12  # SupTitle & Xlabel
+recordWidth = 14
 
-print("=================================|Select_DataFrame_Source|========================1==============")
-
-df1 = pd.read_csv("./../6dataXYZ/ucihar4x.csv")
-df1 = pd.DataFrame(df1)
 kalas = ['palegreen', 'orange', 'yellow', 'gray', 'skyblue', 'green', 'palegreen', 'orange', 'yellow', 'gray', 'skyblue', 'green',
          'palegreen', 'orange', 'yellow', 'gray', 'skyblue', 'green', 'palegreen', 'orange', 'yellow', 'gray', 'skyblue', 'green',
          'palegreen', 'orange', 'yellow', 'gray', 'skyblue', 'green', 'palegreen', 'orange', 'yellow', 'gray', 'skyblue', 'green',
@@ -26,7 +22,9 @@ kalas = ['palegreen', 'orange', 'yellow', 'gray', 'skyblue', 'green', 'palegreen
          'palegreen', 'orange', 'yellow', 'gray', 'skyblue', 'green', 'palegreen', 'orange', 'yellow', 'gray', 'skyblue', 'green',
          'palegreen', 'orange', 'yellow', 'gray', 'skyblue', 'green', 'palegreen', 'orange', 'yellow', 'gray', 'skyblue', 'green']
 
-print('=======================================|Activity_Counter|==========================2==============')
+print("=================================|TABLE1_TRAINING_DATA|========================1==============")
+df1 = pd.read_csv("./../6dataXYZ/ucihar4x.csv")
+df1 = pd.DataFrame(df1)
 A1 = len(df1.loc[df1['ActivityName'].isin(['LAYING'])])
 A2 = len(df1.loc[df1['ActivityName'].isin(['STANDING'])])
 A3 = len(df1.loc[df1['ActivityName'].isin(['SITTING'])])
@@ -34,14 +32,15 @@ A4 = len(df1.loc[df1['ActivityName'].isin(['WALKING'])])
 A5 = len(df1.loc[df1['ActivityName'].isin(['WALKING_DOWNSTAIRS'])])
 A6 = len(df1.loc[df1['ActivityName'].isin(['WALKING_UPSTAIRS'])])
 AZ = [A1, A2, A3, A4, A5, A6]
+
 print('=======================================|Limiting_Number_of_Columns|=================3===============')
-df2 = df1.iloc[0:998877128:, -recordsHieght:]  # 18-Records, last-TEN headers
+
+df2 = df1.iloc[0:998877128:, -recordWidth:]  # 18-Records, last-TEN headers
 df7 = df2.duplicated(subset='ActivityName', keep='last')
 df3 = df2.drop_duplicates(subset=["ActivityName"])
 df3.drop('subject', inplace=True, axis=1)
 df3['Records Per Activity'] = AZ
 df3["ActivityName"] = df3["ActivityName"].str.wrap(10)
-print('=======================================|TITLE_Number_TITLE|========================4===============')
 # df3.sort_values(by=['Activity'], inplace=True)
 df3.sort_values(by=['ActivityName'], inplace=True)
 df3.sort_values(by=['Records Per Activity'], inplace=True)
@@ -49,8 +48,11 @@ df3.style.set_table_styles([dict(selector="th", props=[('max-width', '3px')])])
 
 print('=======================================|TITLE_WRAPPING_ONE|========================5===============')
 
+
 def wrap(string, max_width):
     return '\n'.join(textwrap.wrap(string, max_width))
+
+
 currentHeaderX = df3.columns
 columnHeaderx_new = []
 chomax = 0
@@ -62,9 +64,6 @@ df3.columns = columnHeaderx_new
 df3 = df3.round(3)
 l = len(df3)
 c = len(df3.columns)
-print(df1.shape)
-print(df2.shape)
-print(df3.shape)
 
 fig, ax = plt.subplots(figsize=(18, 2))
 fig.patch.set_visible(False)
@@ -75,15 +74,15 @@ ax1 = ax.table(cellText=df3.values, colLabels=df3.columns,
 ax1 = ax.table(cellText=df3.values, colLabels=df3.columns,
                loc='center', fontsize=15)
 ax1.auto_set_font_size(False)
-ax1.set_fontsize(10)
-plt.title('DATASET_UCIHAR_TRAINING | # Headers= '+str(c) + ' / '+str(len(df1.columns)) +
-          ': # Records='+str(l)+'/'+str(len(df1))+'|', fontsize=fondSizing, color='green', fontweight="bold")
-plt.tight_layout()
-fig.tight_layout()
+ax1.set_fontsize(fontSizing)
+plt.title('1-DATASET_UCIHAR_TRAINING | # Headers= '+str(c) + ' / '+str(len(df1.columns)) +
+          ': # Records='+str(l)+'/'+str(len(df1))+'|', fontsize=fontSizing, color='green', fontweight="bold")
+# plt.tight_layout()
+# fig.tight_layout()
 plt.savefig('../UXviews/tables/TC1.png')
 plt.show()
 
-print('=======================================|Table_TESTING_DATA|==========================7T1================')
+print('=======================================|TABLE2_TESTING_DATA|==========================7T1================')
 
 df1 = pd.read_csv("./../6dataxyz/ucihart2.csv")
 A1 = len(df1.loc[df1['ActivityName'].isin(['LAYING'])])
@@ -93,12 +92,10 @@ A4 = len(df1.loc[df1['ActivityName'].isin(['WALKING'])])
 A5 = len(df1.loc[df1['ActivityName'].isin(['WALKING_DOWNSTAIRS'])])
 A6 = len(df1.loc[df1['ActivityName'].isin(['WALKING_UPSTAIRS'])])
 AZ = [A1, A2, A3, A4, A5, A6]
-print(sum(AZ))
-print(df1.shape)
 
 print('=======================================|Limiting_Number_of_Columns|=================8T2===============')
 
-df2 = df1.iloc[0:2947:, -recordsHieght:]  # 18-Records, last-TEN headers
+df2 = df1.iloc[0:2947:, -recordWidth:]  # 18-Records, last-TEN headers
 df7 = df2.duplicated(subset='ActivityName', keep='last')
 df3 = df2.drop_duplicates(subset=["ActivityName"])
 df3.drop('subject', inplace=True, axis=1)
@@ -110,8 +107,11 @@ df3["ActivityName"] = df3["ActivityName"].str.wrap(10)
 
 print('=======================================|TITLE_WRAPPING_TWO|========================9T3===============')
 
+
 def wrap(string, max_width):
     return '\n'.join(textwrap.wrap(string, max_width))
+
+
 currentHeaderX = df3.columns
 columnHeaderx_new = []
 chomax = 0
@@ -120,7 +120,7 @@ for pemba in currentHeaderX:
     columnHeaderx_new.append(wrap(s, 10))
     chomax += 1
 df3.columns = columnHeaderx_new
-df3 = df3.round(9)
+df3 = df3.round(3)
 l = len(df3)
 c = len(df3.columns)
 
@@ -133,13 +133,15 @@ ax1 = ax.table(cellText=df3.values, colLabels=df3.columns,
 ax1 = ax.table(cellText=df3.values, colLabels=df3.columns,
                loc='center', fontsize=15)
 ax1.auto_set_font_size(False)
-ax1.set_fontsize(12)
-plt.title('DATASET_UCIHAR_TESTING | # Headers= '+str(c) + ' / '+str(len(df1.columns)) +
-          ': # Records='+str(l)+'/'+str(len(df1))+'|', fontsize=fondSizing, color='green', fontweight="bold")
-
-plt.tight_layout()
-fig.tight_layout()
+ax1.set_fontsize(fontSizing)
+plt.title('2-DATASET_UCIHAR_TESTING | # Headers= '+str(c) + ' / '+str(len(df1.columns)) +
+          ': # Records='+str(l)+'/'+str(len(df1))+'|', fontsize=fontSizing, color='green', fontweight="bold")
+# plt.tight_layout()
+# fig.tight_layout()
 plt.savefig('../UXviews/tables/TC2.png')
 plt.show()
 
 print("===================================|UCIHAR_Plotting_completed_Successufly|==============11===========")
+print(df1.shape)
+print(df2.shape)
+print(df3.shape)
